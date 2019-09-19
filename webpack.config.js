d5073bf8be48d54e811aaae39fedcc0d68048907
kazuki.kawabata <>
2019-09-19 23:21:46 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, argv) => ({
+  entry: './src/scripts/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    watchContentBase: true,
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        use: ['url-loader']
+      },
+      {
+        test: /\.html$/,
+        use: 'html-loader'
+      }
+    ]
+  },
+  plugins: [
+    new UglifyJSPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+});
